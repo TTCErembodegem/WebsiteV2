1ba651049c98eaf774919f79b42bf4e866499693
Laoujin <woutervs@hotmail.com>
2015-08-27 23:41:26 +0200

    Working Dtos project

---
+++ SiteTTCErembodegem/src/Ttc.Dtos/TtcConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ttc.Dtos
+{
+    /// <summary>
+    /// Global configuration for TTC Erembodegem
+    /// </summary>
+    public class TtcConfiguration
+    {
+        public TimeSpan StandardCompetitionStart { get; set; }
+    }
+}
--- SiteTTCErembodegem/src/Ttc.Site/Controllers/ConfigController.cs
+++ SiteTTCErembodegem/src/Ttc.Site/Controllers/ConfigController.cs
@@ -1,19 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNet.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Ttc.Dtos;
 
 namespace Ttc.Site.Controllers
 {
+    [Route("api/[controller]")]
     public class ConfigController : ApiController
     {
-        //public TtcConfiguration Get()
-        //{
-        //    return new TtcConfiguration
-        //    {
-        //        StandardCompetitionStart = new TimeSpan(20, 0, 0)
-        //    };
-        //}
+        public TtcConfiguration Get()
+        {
+            return new TtcConfiguration
+            {
+                StandardCompetitionStart = new TimeSpan(20, 0, 0)
+            };
+        }
     }
 }
